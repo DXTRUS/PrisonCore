e0e6ffc9805cd2cf5eeba54f5c2625d40048b0ec
ProdPreva1l <prod.preva1l@gmail.com>
2024-09-01 13:36:20 +1000

    no more offhand for pickaxes

--- src/main/java/us/dxtrus/prisoncore/listeners/PickaxeListeners.java
+++ src/main/java/us/dxtrus/prisoncore/listeners/PickaxeListeners.java
@@ -4,6 +4,7 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.event.player.PlayerDropItemEvent;
+import org.bukkit.event.player.PlayerSwapHandItemsEvent;
 import us.dxtrus.prisoncore.PickaxeManager;
 
 public class PickaxeListeners implements Listener {
@@ -17,6 +18,13 @@ public class PickaxeListeners implements Listener {
     @EventHandler
     public void onItemDrop(PlayerDropItemEvent e) {
         if (PickaxeManager.isPickaxe(e.getItemDrop().getItemStack())) {
+            e.setCancelled(true);
+        }
+    }
+
+    @EventHandler
+    public void onOffhand(PlayerSwapHandItemsEvent e) {
+        if (PickaxeManager.isPickaxe(e.getMainHandItem())) {
             e.setCancelled(true);
         }
     }
